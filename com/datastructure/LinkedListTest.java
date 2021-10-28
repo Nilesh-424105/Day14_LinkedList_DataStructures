7fd133173831137db339fb6cee9c7023c80a2fb1
Nilesh-424105 <89961130+Nilesh-424105@users.noreply.github.com>
2021-10-28 10:00:09 +0530

    ADDED: Welcome Message

---
+++ com/datastructure/LinkedListTest.java
@@ -0,0 +1,10 @@
+package com.datastructure;
+
+public class LinkedListTest {
+
+	public static void main(String[] args) {
+
+		System.out.println("Welcome To Linked List Computation Program");
+	}
+
+}
